2fb278cdea63a2bd6ce714c57cd968276d55436e
fazt <fazt@faztweb.com>
2020-03-14 17:55:01 -0500

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import {Server, Request, ResponseToolkit} from '@hapi/hapi'
+
+export const routes = (server: Server) => {
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request: Request, h: ResponseToolkit) => {
+      return 'hello world';
+    }
+  });
+
+}
